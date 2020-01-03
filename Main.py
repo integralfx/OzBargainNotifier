e3aa4a26bd6d95f7311680f57133f50232782852
integralfx <jordansu83@hotmail.com>
2020-01-03 17:15:42 +1100

    Save sales to database

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import xml.etree.ElementTree as ET
 from datetime import datetime
 import pytz
 import sqlite3
+from tqdm import tqdm
 
 def get_ebay_sales():
     res = requests.get('https://www.ozbargain.com.au/tag/ebay-sale/feed')
@@ -18,23 +19,31 @@ def get_ebay_sales():
         sale['description'] = item.find('description').text
         link = item.find('link').text
         sale['id'] = int(link[link.find('node/') + len('node/'):])
+        sale['date_published'] = datetime.strptime(item.find('pubDate').text, '%a, %d %b %Y %H:%M:%S %z')
 
         meta = item.find('{https://www.ozbargain.com.au}meta')
         sale['link'] = meta.get('link')
         sale['expiry'] = datetime.fromisoformat(meta.get('expiry'))
+
+        sale['creator'] = item.find('{http://purl.org/dc/elements/1.1/}creator').text
 
         sales.append(sale)
 
     return sales
 
 
-if __name__ == '__main__':
-    sales = get_ebay_sales()
+def save_sales(sales):
     with sqlite3.connect('sales.db') as conn:
-        sql = 'INSERT OR REPLACE INTO SalesUpdate(id, last_update) VALUES'
-        for sale in sales:
-            print(f'{sale["id"]} - {sale["title"]}')
-            sql += f'({sale["id"]}, "{datetime.utcnow().isoformat()}"), '
+        sql = 'INSERT OR REPLACE INTO Sales(id, last_update, title, link, expiry, date_published, creator) VALUES'
+        pbar = tqdm(sales, desc='Saving sales', unit='sales')
+        for sale in pbar:
+            pbar.set_postfix(id=sale['id'], title=sale['title'])
+            sql += f'({sale["id"]}, "{datetime.utcnow().isoformat()}", "{sale["title"]}", "{sale["link"]}", \
+                     "{sale["expiry"].isoformat()}", "{sale["date_published"].isoformat()}", "{sale["creator"]}"), '
 
         cur = conn.cursor()
         cur.execute(sql[:-2])
+
+
+if __name__ == '__main__':
+    save_sales(get_ebay_sales())
