caa782d0e8a47a14836af5e7bbb0147ad3942b57
integralfx <jordansu83@hotmail.com>
2020-01-04 16:22:21 +1100

    Add load_sales()

--- Main.py
+++ Main.py
@@ -3,7 +3,9 @@ import xml.etree.ElementTree as ET
 from datetime import datetime
 import pytz
 import sqlite3
-from tqdm import tqdm
+
+SALES_DB = 'sales.db'
+conn = sqlite3.connect(SALES_DB)
 
 def get_ebay_sales():
     res = requests.get('https://www.ozbargain.com.au/tag/ebay-sale/feed')
@@ -33,11 +35,9 @@ def get_ebay_sales():
 
 
 def save_sales(sales):
-    with sqlite3.connect('sales.db') as conn:
+    with conn:
         sql = 'INSERT OR REPLACE INTO Sales(id, last_update, title, link, expiry, date_published, creator) VALUES'
-        pbar = tqdm(sales, desc='Saving sales', unit='sales')
-        for sale in pbar:
-            pbar.set_postfix(id=sale['id'], title=sale['title'])
+        for sale in sales:
             sql += f'({sale["id"]}, "{datetime.utcnow().isoformat()}", "{sale["title"]}", "{sale["link"]}", \
                      "{sale["expiry"].isoformat()}", "{sale["date_published"].isoformat()}", "{sale["creator"]}"), '
 
@@ -45,5 +45,28 @@ def save_sales(sales):
         cur.execute(sql[:-2])
 
 
+def load_sales():
+    with conn:
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM Sales')
+        sales = []
+        for row in cur.fetchall():
+            sale = {
+                'id': int(row[0]),
+                'last_update': datetime.fromisoformat(row[1]),
+                'title': row[2],
+                'link': row[3],
+                'expiry': datetime.fromisoformat(row[4]),
+                'date_published': datetime.fromisoformat(row[5]),
+                'creator': row[6]
+            }
+            sales.append(sale)
+
+        return sales
+
+
 if __name__ == '__main__':
-    save_sales(get_ebay_sales())
+    for sale in load_sales():
+        if datetime.now(tz=pytz.UTC) > sale['expiry']:
+            print(f'{"[Expired]"} {sale["id"]}')
+        #print(f'{"Expired " if datetime.utcnow() > sale["expiry"] else ""}{sale["id"]}')
