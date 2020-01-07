e5278a9d67e02990f3429bbaf2cfe0ff28d3c960
integralfx <jordansu83@hotmail.com>
2020-01-07 21:09:52 +1100

    Create create_tables.sql

---
+++ create_tables.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS Sales (
+	id	INTEGER PRIMARY KEY,
+	last_update	TEXT,
+	title	TEXT,
+	link	TEXT,
+	expiry	TEXT,
+	date_published	TEXT,
+	creator	TEXT
+);
+
+CREATE TABLE IF NOT EXISTS Category (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    link TEXT,
+);
+
+CREATE TABLE IF NOT EXISTS SalesCategory (
+    sales_id INTEGER,
+    category_id INTEGER,
+    PRIMARY KEY(sales_id, category_id),
+    FOREIGN KEY(sales_id) REFERENCES Sale(id),
+    FOREIGN KEY(category_id) REFERENCES Category(id)
+);
